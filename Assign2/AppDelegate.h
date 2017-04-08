30bc15a08d92899321f1fc196419b5bf8cbb33c8
Lan David <David@10-100-252-179-eduroam.student.utas.edu.au>
2017-04-08 13:22:15 +1000

    Initial Commit

---
+++ Assign2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Assign2
+//
+//  Created by David Lan on 8/4/17.
+//  Copyright © 2017 David Lan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Assign2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Assign2
+//
+//  Created by David Lan on 8/4/17.
+//  Copyright © 2017 David Lan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
